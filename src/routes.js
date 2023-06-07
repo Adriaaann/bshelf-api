ef7dc11c619e5d00200b7fd8e3befc4abcd47db2
Adriaaann <adriansouza04@gmail.com>
2023-06-07 15:39:10 -0300

    clean up

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,6 @@ const routes = express.Router();
 
 const UsersController = require('./controllers/UsersController');
 const BooksController = require('./controllers/BooksController');
-const PlanningController = require('./controllers/PlanningController');
-const FavoritesController = require('./controllers/FavoritesController');
 
 routes.post('/users', UsersController.create);
 routes.get('/users', UsersController.read);
@@ -13,10 +11,6 @@ routes.get('/users/:id', UsersController.readOne);
 routes.delete('/users/:id', UsersController.delete);
 
 routes.post('/login', UsersController.login);
-
-routes.get('/favorites/:id', FavoritesController.read);
-
-routes.get('/planning/:id', PlanningController.read);
 
 routes.get('/books/:id', BooksController.read);
 routes.get('/books/:id/:bookId', BooksController.readOne);
